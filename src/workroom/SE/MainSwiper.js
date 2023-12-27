92b6ade5aa89f22edc492614c64d349477b390da
BOOK-9SCGP5I2KQ\andyc <1000tmddms@gmail.com>
2023-12-27 15:42:24 +0900

    데이터에 구분자 추가하여 br태그 구현, 스와이퍼 영어 텍스트 추가

--- src/workroom/SE/MainSwiper.js
+++ src/workroom/SE/MainSwiper.js
@@ -3,6 +3,7 @@ import { EffectFade, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/effect-fade';
 import mainS from '../../data/MainSwiper.json'
+import React from 'react';
 
 
 export default (props) => {
@@ -18,19 +19,26 @@ export default (props) => {
 
                 spaceBetween={50}
                 slidesPerView={1}
-                // onSlideChange={() => console.log('slide change')}
-                // onSwiper={(swiper) => console.log(swiper)}
+            // onSlideChange={() => console.log('slide change')}
+            // onSwiper={(swiper) => console.log(swiper)}
 
             >
                 {
                     props.datasrc.map((el, idx) => {
                         return <SwiperSlide style={{
-                            background: `url(/assets/img/swiper_img/${el.background}) no-repeat center`, height: "480px", backgroundSize: "auto 100%"
+                            background: `url(./assets/img/swiper_img/${el.background}) no-repeat center`, height: "480px", backgroundSize: "auto 100%"
                         }} key={idx}>
                             <div className='effecttext'>
 
                                 <strong>{el.strong}</strong>
-                                <p>{el.Mtext}</p>
+                                <p>{el.Mtext.split("|").map((e, i) => {
+                                    return (
+                                        <React.Fragment key={i}>
+                                            {e}
+                                            <br />
+                                        </React.Fragment>
+                                    )
+                                })}</p>
                             </div >
 
                         </SwiperSlide >
